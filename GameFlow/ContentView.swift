de4cea6e05d28fe1ffa31624e3b46ff035ce5430
YaroslavSokolov0372 <yaroslav.sokolov0372@gmail.com>
2023-10-09 20:23:29 +0200

    Initial Commit

---
+++ GameFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GameFlow
+//
+//  Created by Yaroslav Sokolov on 09/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GameFlow/GameFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameFlowApp.swift
+//  GameFlow
+//
+//  Created by Yaroslav Sokolov on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GameFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
